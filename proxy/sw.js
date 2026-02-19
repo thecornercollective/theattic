77275d2266bef046c84670a1b57d988c8c723635
twentyfolds <beenthatbray@gmail.com>
2026-02-19 12:21:50 -0500

    Create sw.js

---
+++ proxy/sw.js
@@ -0,0 +1,26 @@
+/// <reference path="../lib/index.d.ts" />
+
+// dumb hack to allow firefox to work (please dont do this in prod)
+if (navigator.userAgent.includes("Firefox")) {
+	Object.defineProperty(globalThis, "crossOriginIsolated", {
+		value: true,
+		writable: false,
+	});
+}
+
+importScripts("https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@1.2.2/scramjet.all.js");
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const scramjet = new ScramjetServiceWorker();
+
+async function handleRequest(event) {
+	await scramjet.loadConfig();
+	if (scramjet.route(event)) {
+		return scramjet.fetch(event);
+	}
+
+	return fetch(event.request);
+}
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event));
+});
